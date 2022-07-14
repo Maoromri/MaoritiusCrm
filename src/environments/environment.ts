6b4feac1975409eac405afd885b4bcb43ba357e5
maor omri <maor.hightech@gmail.com>
2022-07-14 18:13:39 +0300

    MaoritiusCrm

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,6 +3,14 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'crm-project-ang',
+    appId: '1:829835360854:web:cbc4bf07d77066b43b19f7',
+    storageBucket: 'crm-project-ang.appspot.com',
+    apiKey: 'AIzaSyDlMcD7r22nLsFwXoeUG8US_94Po4Ipw-Q',
+    authDomain: 'crm-project-ang.firebaseapp.com',
+    messagingSenderId: '829835360854',
+  },
   production: false
 };
 
